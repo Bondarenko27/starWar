233ba74617c6405f3c6f855773b270889d49e2e8
Kristina Bondarenko <bondarenkokristina27@gmail.com>
2021-09-17 12:39:48 -0400

    add read operation and rendering html

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
+//mongodb+srv://Bondarenko27:<password>@cluster0.euqup.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const express = require('express');
 const bodyParser= require('body-parser');
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(3000, function() {
-    console.log('listening on 3000')
-  });
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-  });
-  app.post('/quotes', (req, res) => {
-    console.log(req.body)
-  });
+const MongoClient = require('mongodb').MongoClient
+MongoClient.connect('mongodb+srv://Bondarenko27:Programer$21@cluster0.euqup.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+{useUnifiedTopology: true}).then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.set('view engine', 'ejs')
+    app.get('/', (req, res) => {
+        db.collection('quotes').find().toArray()
+        .then(results => {
+          console.log(results)})
+        res.sendFile(__dirname + '/index.html')})
+    app.post('/quotes', (req, res) => {
+            quotesCollection.insertOne(req.body)
+              .then(result => {
+                res.redirect('/')
+              })
+            })
+    app.listen(3000, function() {
+        console.log('listening on 3000')
+      })
+  })
+  .catch(error => console.error(error))
+  
+
+
+  
+  
   
